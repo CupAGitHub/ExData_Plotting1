5915b2f5c272b6f9ff4d3236680312e12e7a755d
Michael McLaughlin <michaelmclaughlin@mclaughlin-mbp.local>
2016-09-28 11:55:22 -0400

    scriptify

--- plot1.R
+++ plot1.R
@@ -1,7 +1,4 @@
 plotOne <- function() {
-  
-  dev.new(width=480, height=480)
-  
   # Read the data
   df <- read.table("power.txt", header = FALSE, sep = ";") 
   names(df) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
@@ -17,8 +14,12 @@ plotOne <- function() {
   extractor <- c(TRUE, FALSE, FALSE, FALSE)
   axis(1, bp, labels=c((lapply(as.numeric(names(groups)), function(x) x/2 - .5))[extractor]), at=c(c(0:max(values))[extractor]))
   
-  dev.copy(png,'plot1.png')
-  dev.off()
 } 
 
- 
+
+dev.new(width=480, height=480)
+
+plotOne()
+
+dev.copy(png,'plot1.png')
+dev.off()
